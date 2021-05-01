c98c66f76706539bb63a91dc5e6dbb910997888a
Richard <richard.yang.tong@gmail.com>
2021-05-01 08:33:52 -0400

    Added five states.

--- src/academy/learnprogramming/BankAccount.java
+++ src/academy/learnprogramming/BankAccount.java
@@ -1,5 +1,9 @@
 package academy.learnprogramming;
 
 public class BankAccount {
-    
+    private int accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
 }
