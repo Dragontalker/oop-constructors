617908fccd0468f4fdf2f5ee796729b6ee3529e2
Richard <richard.yang.tong@gmail.com>
2021-05-01 08:32:25 -0400

    Added a new class BankAccount.

---
+++ src/academy/learnprogramming/BankAccount.java
@@ -0,0 +1,5 @@
+package academy.learnprogramming;
+
+public class BankAccount {
+    
+}
--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,8 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Challenge
+
     }
 }
