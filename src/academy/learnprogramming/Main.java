c9c3cebb63ac8ea6259fae2702ff6517ac01399c
Richard <richard.yang.tong@gmail.com>
2021-05-01 08:46:48 -0400

    Added tests.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         // Challenge
-
+        BankAccount myAccount = new BankAccount();
+        myAccount.setAccountNumber(2324);
+        myAccount.setBalance(2000.00d);
+        myAccount.setCustomerName("Richard");
+        myAccount.deposit(-400d);
+        myAccount.deposit(4000d);
+        myAccount.withdraw(6000d);
     }
 }
